b51363977e2dd773a7bd42b35c5b7bdd422988a5
mduplifting <mduplifting@gmail.com>
2025-11-19 18:31:32 +0200

    Revert old package name to GCDWebServer

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "GCDWebServers",
-            targets: ["GCDWebServers"]),
+            name: "GCDWebServer",
+            targets: ["GCDWebServer"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "GCDWebServers",
+            name: "GCDWebServer",
             dependencies: [],
             path: ".",
             exclude: [
@@ -45,8 +45,8 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "GCDWebServersTests",
-            dependencies: ["GCDWebServers"],
+            name: "GCDWebServerTests",
+            dependencies: ["GCDWebServer"],
             path: "Frameworks",
             exclude: [
                 "GCDWebServers.h",
